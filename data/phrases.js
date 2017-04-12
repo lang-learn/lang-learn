41f937202a958ebb4452911fc32d6273647661c4
Michael Pszonka <pszonkadonk@host-155-246-211-136.dhcp.stevens-tech.edu>
2017-04-12 16:55:33 -0400

    Add sidebar, menu selection

--- data/phrases.js
+++ data/phrases.js
@@ -1,4 +1,4 @@
-    let pleasantries = [
+    let greeting = [
         "Hello",
         "Goodbye",
         "Please",
@@ -11,25 +11,156 @@
         "How are you",
     ];
 
-    let inquiries = [
-        "Do you speak English?",
-        "May I have something to eat",
-        "Where is the restroom?",
-        "Where is my gate",
-        "How much does this cost",
-        "What time is it?",
-        "Where can I find a place to eat",
-        "How far is it to",
-        "Could I have a drink",
-        "I would like to order",
+    let directions = [
+        "How do I get to",
+        "How far is",
+        "Where can I find the closest",
+        "Do you have a map",
+        "Where is",
+        "Can you show me on the map",
+        "Coud you point me in the direction",
+        "I'm lost"
+    ];
+
+    let emergency = [
+        "Help",
+        "I need a doctor",
         "I need help",
-        "Where can I find the train",
+        "Can I use your phone",
+        "Please call the police"
+    ];
+
+    let atHotel = [
+        "I have a reservation",
+        "Do you have any rooms available",
+        "What time is check in",
+        "What time is check out",
+        "I would like to book a room",
+        "Could you recommend some restaurants"
+    ];
+
+    let atAirport = [
+        "What platform is my plane",
+        "What gate is my plane",
+        "Is there a bus at the airport",
+        "I have my passport",
+        "Is this seat taken",
+        "Are there any delays"
+    ];
+
+    let atRestaurant = [
+        "Are there any specials",
+        "Could you recommend something",
+        "Could I have a beer",
+        "Could I have water",
+        "Can I have another drink",
+        "This is great",
+        "This is not what I ordered"
+    ];
+
+    let transportation = [
+        "Are there any taxis",
+        "Could you take me to",
+        "I'd like to go to",
+        "Could you let me know when to get off",
+        "Does this go to",
+        "Is this the correct bus to get to"
+    ];
+
+    let sightSeeing = [
+        "Home much does it cost",
+        "Do I need tickets",
+        "That is beautiful",
+        "What is there to see around here"
+    ];
+
+    let shoppingAndMoney = [
+        "How much does this cost",
+        "Could I please have",
+        "Do you have anything cheaper",
+        "Can I pay with a credit card",
+        "Do you accept credit cards",
+        "Could I have change for a",
+        "What time do you close"
+    ];
+    
+    let allInquiries = [
+        "Hello",
+        "Goodbye",
+        "Please",
+        "Thank you",
+        "I'm sorry",
+        "Excuse me",
+        "My name is",
+        "Good Morning",
+        "Good Afternoon",
+        "How are you",
+        "How do I get to",
+        "How far is",
+        "Where can I find the closest",
+        "Do you have a map",
+        "Where is",
+        "Can you show me on the map",
+        "Coud you point me in the direction",
         "I'm lost",
-        "Do you take credit cards",
-    ]
+        "Help",
+        "I need a doctor",
+        "I need help",
+        "Can I use your phone",
+        "Please call the police",
+        "I have a reservation",
+        "Do you have any rooms available",
+        "What time is check in",
+        "What time is check out",
+        "I would like to book a room",
+        "Could you recommend some restaurants",
+        "What platform is my plane",
+        "What gate is my plane",
+        "Is there a bus at the airport",
+        "I have my passport",
+        "Is this seat taken",
+        "Are there any delays",
+        "Are there any specials",
+        "Could you recommend something",
+        "Could I have a beer",
+        "Could I have water",
+        "Can I have another drink",
+        "This is great",
+        "This is not what I ordered",
+        "Are there any taxis",
+        "Could you take me to",
+        "I'd like to go to",
+        "Could you let me know when to get off",
+        "Does this go to",
+        "Is this the correct bus to get to",
+        "Are there any taxis",
+        "Could you take me to",
+        "I'd like to go to",
+        "Could you let me know when to get off",
+        "Does this go to",
+        "Is this the correct bus to get to",
+        "How much does this cost",
+        "Could I please have",
+        "Do you have anything cheaper",
+        "Can I pay with a credit card",
+        "Do you accept credit cards",
+        "Could I have change for a",
+        "What time do you close"   
+    ];
+
+
+
 
 module.exports = {
-    pleasantries: pleasantries,
-    inquiries: inquiries
+    greeting: greeting,
+    directions: directions,
+    emergency: emergency,
+    atHotel: atHotel,
+    atAirport: atAirport,
+    atRestaurant: atRestaurant,
+    transportation: transportation,
+    sightSeeing: sightSeeing,
+    shoppingAndMoney: shoppingAndMoney,
+    allInquiries: allInquiries
 };
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const apiKey = "AIzaSyD9OreuZNrNyDAqQBeKgzsDfLPA18IPdDc";
 const gTranslate = require('google-translate')(apiKey);
 const data = require('../data/phrases');
-const pleasantries = data.pleasantries;
 
 var langs = {
     'auto': 'Automatic',
@@ -136,9 +135,47 @@ const constructorMethod = (app) => {
 
     app.get("/newWord/:tLangCode", (req, res) => {
         let tLangCode = req.params.tLangCode;
-        let phrase = pleasantries[Math.floor(Math.random() * pleasantries.length)]
+        let phrase = data.allInquiries[Math.floor(Math.random() * data.allInquiries.length)]
         res.redirect(`/learn/${tLangCode}/${phrase}`);
     })
+
+    app.get("/newWord/:phraseType/:tLangCode", (req, res) => {
+    let tLangCode = req.params.tLangCode;
+    let phraseType = req.params.phraseType;
+    let phrase;
+
+
+    switch(phraseType) {
+        case "greeting":
+            phrase = data.greeting[Math.floor(Math.random() * data.greeting.length)]
+            break;
+        case "directions":
+            phrase = data.directions[Math.floor(Math.random() * data.directions.length)]
+            break;
+        case "emergency":
+            phrase = data.emergency[Math.floor(Math.random() * data.emergency.length)]
+            break;
+        case "atHotel":
+            phrase = data.atHotel[Math.floor(Math.random() * data.atHotel.length)]
+            break;
+        case "atRestaurant":
+            phrase = data.atRestaurant[Math.floor(Math.random() * data.atRestaurant.length)]
+            break;
+        case "atAirport":
+            phrase = data.atAirport[Math.floor(Math.random() * data.atAirport.length)]
+            break;
+        case "transportation":
+            phrase = data.transportation[Math.floor(Math.random() * data.transportation.length)]
+            break;
+        case "sightSeeing":
+            phrase = data.sightSeeing[Math.floor(Math.random() * data.sightSeeing.length)]
+            break;
+        case "shoppingAndMoney":
+            phrase = data.shoppingAndMoney[Math.floor(Math.random() * data.shoppingAndMoney.length)]
+            break;
+    }
+    res.redirect(`/learn/${tLangCode}/${phrase}`);
+});
 
     app.use("*", (req, res) => {
         res.sendStatus(404);
