e0ae4522911969fcf23ad2a7216557126455afea
Josh Gribbon <jgribbon@stevens.edu>
2017-04-13 23:42:38 -0400

    Create 'langs' data file, refactor switch statement in routing, fix typo in a link

--- data/phrases.js
+++ data/phrases.js
@@ -1,152 +1,152 @@
-    let greeting = [
-        "Hello",
-        "Goodbye",
-        "Please",
-        "Thank you",
-        "I'm sorry",
-        "Excuse me",
-        "My name is",
-        "Good Morning",
-        "Good Afternoon",
-        "How are you",
-    ];
+let greeting = [
+    "Hello",
+    "Goodbye",
+    "Please",
+    "Thank you",
+    "I'm sorry",
+    "Excuse me",
+    "My name is",
+    "Good Morning",
+    "Good Afternoon",
+    "How are you",
+];
 
-    let directions = [
-        "How do I get to",
-        "How far is",
-        "Where can I find the closest",
-        "Do you have a map",
-        "Where is",
-        "Can you show me on the map",
-        "Coud you point me in the direction",
-        "I'm lost"
-    ];
+let directions = [
+    "How do I get to",
+    "How far is",
+    "Where can I find the closest",
+    "Do you have a map",
+    "Where is",
+    "Can you show me on the map",
+    "Coud you point me in the direction",
+    "I'm lost"
+];
 
-    let emergency = [
-        "Help",
-        "I need a doctor",
-        "I need help",
-        "Can I use your phone",
-        "Please call the police"
-    ];
+let emergency = [
+    "Help",
+    "I need a doctor",
+    "I need help",
+    "Can I use your phone",
+    "Please call the police"
+];
 
-    let atHotel = [
-        "I have a reservation",
-        "Do you have any rooms available",
-        "What time is check in",
-        "What time is check out",
-        "I would like to book a room",
-        "Could you recommend some restaurants"
-    ];
+let atHotel = [
+    "I have a reservation",
+    "Do you have any rooms available",
+    "What time is check in",
+    "What time is check out",
+    "I would like to book a room",
+    "Could you recommend some restaurants"
+];
 
-    let atAirport = [
-        "What platform is my plane",
-        "What gate is my plane",
-        "Is there a bus at the airport",
-        "I have my passport",
-        "Is this seat taken",
-        "Are there any delays"
-    ];
+let atAirport = [
+    "What platform is my plane",
+    "What gate is my plane",
+    "Is there a bus at the airport",
+    "I have my passport",
+    "Is this seat taken",
+    "Are there any delays"
+];
 
-    let atRestaurant = [
-        "Are there any specials",
-        "Could you recommend something",
-        "Could I have a beer",
-        "Could I have water",
-        "Can I have another drink",
-        "This is great",
-        "This is not what I ordered"
-    ];
+let atRestaurant = [
+    "Are there any specials",
+    "Could you recommend something",
+    "Could I have a beer",
+    "Could I have water",
+    "Can I have another drink",
+    "This is great",
+    "This is not what I ordered"
+];
 
-    let transportation = [
-        "Are there any taxis",
-        "Could you take me to",
-        "I'd like to go to",
-        "Could you let me know when to get off",
-        "Does this go to",
-        "Is this the correct bus to get to"
-    ];
+let transportation = [
+    "Are there any taxis",
+    "Could you take me to",
+    "I'd like to go to",
+    "Could you let me know when to get off",
+    "Does this go to",
+    "Is this the correct bus to get to"
+];
 
-    let sightSeeing = [
-        "Home much does it cost",
-        "Do I need tickets",
-        "That is beautiful",
-        "What is there to see around here"
-    ];
+let sightSeeing = [
+    "Home much does it cost",
+    "Do I need tickets",
+    "That is beautiful",
+    "What is there to see around here"
+];
 
-    let shoppingAndMoney = [
-        "How much does this cost",
-        "Could I please have",
-        "Do you have anything cheaper",
-        "Can I pay with a credit card",
-        "Do you accept credit cards",
-        "Could I have change for a",
-        "What time do you close"
-    ];
-    
-    let allInquiries = [
-        "Hello",
-        "Goodbye",
-        "Please",
-        "Thank you",
-        "I'm sorry",
-        "Excuse me",
-        "My name is",
-        "Good Morning",
-        "Good Afternoon",
-        "How are you",
-        "How do I get to",
-        "How far is",
-        "Where can I find the closest",
-        "Do you have a map",
-        "Where is",
-        "Can you show me on the map",
-        "Coud you point me in the direction",
-        "I'm lost",
-        "Help",
-        "I need a doctor",
-        "I need help",
-        "Can I use your phone",
-        "Please call the police",
-        "I have a reservation",
-        "Do you have any rooms available",
-        "What time is check in",
-        "What time is check out",
-        "I would like to book a room",
-        "Could you recommend some restaurants",
-        "What platform is my plane",
-        "What gate is my plane",
-        "Is there a bus at the airport",
-        "I have my passport",
-        "Is this seat taken",
-        "Are there any delays",
-        "Are there any specials",
-        "Could you recommend something",
-        "Could I have a beer",
-        "Could I have water",
-        "Can I have another drink",
-        "This is great",
-        "This is not what I ordered",
-        "Are there any taxis",
-        "Could you take me to",
-        "I'd like to go to",
-        "Could you let me know when to get off",
-        "Does this go to",
-        "Is this the correct bus to get to",
-        "Are there any taxis",
-        "Could you take me to",
-        "I'd like to go to",
-        "Could you let me know when to get off",
-        "Does this go to",
-        "Is this the correct bus to get to",
-        "How much does this cost",
-        "Could I please have",
-        "Do you have anything cheaper",
-        "Can I pay with a credit card",
-        "Do you accept credit cards",
-        "Could I have change for a",
-        "What time do you close"   
-    ];
+let shoppingAndMoney = [
+    "How much does this cost",
+    "Could I please have",
+    "Do you have anything cheaper",
+    "Can I pay with a credit card",
+    "Do you accept credit cards",
+    "Could I have change for a",
+    "What time do you close"
+];
+
+let allInquiries = [
+    "Hello",
+    "Goodbye",
+    "Please",
+    "Thank you",
+    "I'm sorry",
+    "Excuse me",
+    "My name is",
+    "Good Morning",
+    "Good Afternoon",
+    "How are you",
+    "How do I get to",
+    "How far is",
+    "Where can I find the closest",
+    "Do you have a map",
+    "Where is",
+    "Can you show me on the map",
+    "Coud you point me in the direction",
+    "I'm lost",
+    "Help",
+    "I need a doctor",
+    "I need help",
+    "Can I use your phone",
+    "Please call the police",
+    "I have a reservation",
+    "Do you have any rooms available",
+    "What time is check in",
+    "What time is check out",
+    "I would like to book a room",
+    "Could you recommend some restaurants",
+    "What platform is my plane",
+    "What gate is my plane",
+    "Is there a bus at the airport",
+    "I have my passport",
+    "Is this seat taken",
+    "Are there any delays",
+    "Are there any specials",
+    "Could you recommend something",
+    "Could I have a beer",
+    "Could I have water",
+    "Can I have another drink",
+    "This is great",
+    "This is not what I ordered",
+    "Are there any taxis",
+    "Could you take me to",
+    "I'd like to go to",
+    "Could you let me know when to get off",
+    "Does this go to",
+    "Is this the correct bus to get to",
+    "Are there any taxis",
+    "Could you take me to",
+    "I'd like to go to",
+    "Could you let me know when to get off",
+    "Does this go to",
+    "Is this the correct bus to get to",
+    "How much does this cost",
+    "Could I please have",
+    "Do you have anything cheaper",
+    "Can I pay with a credit card",
+    "Do you accept credit cards",
+    "Could I have change for a",
+    "What time do you close"
+];
 
 
 
@@ -163,4 +163,3 @@ module.exports = {
     shoppingAndMoney: shoppingAndMoney,
     allInquiries: allInquiries
 };
-
